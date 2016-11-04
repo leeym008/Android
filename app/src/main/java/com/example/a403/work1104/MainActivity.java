18040b8d999196dbad0e79fb6fe8d7c4ca900652
leeym008 <leeym008@naver.com>
2016-11-04 19:47:43 +0900

    Create Helloword Android Project

---
+++ app/src/main/java/com/example/a403/work1104/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a403.work1104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button b1;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
